d4c02f0488585f586ac2771c9aa59f230b9d1422
cieno-naver <cieno@naver.com>
2020-09-14 16:45:27 -0400

    20200914

--- MrcCoreMvc/Controllers/AttendanceController.cs
+++ MrcCoreMvc/Controllers/AttendanceController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MrcCoreMvc.Models;
+using MRCDataLibrary._02_Models;
 using MRCDataLibrary._03_Data;
 
 namespace MrcCoreMvc.Controllers
@@ -11,10 +14,14 @@ namespace MrcCoreMvc.Controllers
     public class AttendanceController : Controller
     {
         private readonly IAttendanceData _attendanceData;
+        private readonly IMemberData _memberData;
+        private readonly IWorshipData _worshipData;
 
-        public AttendanceController(IAttendanceData attendanceData)
+        public AttendanceController(IAttendanceData attendanceData, IMemberData memberData, IWorshipData worshipData)
         {
             _attendanceData = attendanceData;
+            _memberData = memberData;
+            _worshipData = worshipData;
         }
         // GET: AttendanceController
         public async Task <IActionResult> Index()
@@ -25,50 +32,40 @@ namespace MrcCoreMvc.Controllers
         // GET: AttendanceController/Details/5
         public async Task<IActionResult> AttendanceList(string worshipId)
         {
+            var attendanceList = new AttendanceListModel();
             var attendance = await _attendanceData.GetAttendanceByWorship(worshipId);
-            return View(attendance);
+            var worship = await _worshipData.GetWorshipById(worshipId);
+            attendanceList.AttendanceList = attendance;
+            attendanceList.Worship = worship;
+
+            return View(attendanceList);
         }
 
         // GET: AttendanceController/Create
-        public ActionResult Create()
+        public async Task<IActionResult> Create(string worshipId)
         {
-            return View();
+            var members = await _memberData.GetMembers();
+            var attendance = new AttendanceModel();
+            attendance.WORSHIP_ID = worshipId;
+            members.ForEach(x =>
+            {
+                attendance.MEMBER_LIST
+                .Add(item: new SelectListItem { Value = x.MEMBER_ID, Text = x.FIRST_NAME + " " + x.LAST_NAME });
+            });
+            return View(attendance);
         }
 
         // POST: AttendanceController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create(AttendanceModel attendance)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
+            if(ModelState.IsValid == false)
             {
                 return View();
             }
-        }
-
-        // GET: AttendanceController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: AttendanceController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            string id = await _attendanceData.CreateAttendance(attendance);
+            return RedirectToAction("AttendanceList", new { worshipId = id });
         }
 
         // GET: AttendanceController/Delete/5
---
+++ MrcCoreMvc/Models/AttendanceListModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MRCDataLibrary._02_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MrcCoreMvc.Models
+{
+    public class AttendanceListModel
+    {
+        public List<AttendanceModel> AttendanceList { get; set; }
+        public WorshipModel Worship { get; set; }
+    }
+}
